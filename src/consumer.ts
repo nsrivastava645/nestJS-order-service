97e1761b2d59e69496b5d7b1656fd0179b28734f
nsrivastava645 <nsrivastava645@gmail.com>
2024-01-22 05:21:19 +0530

    amqp string in env

--- src/consumer.ts
+++ src/consumer.ts
@@ -2,12 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { Transport } from '@nestjs/microservices';
 import { QUEUES } from './shared/const/messaging.constants';
 import { OrderMessageConsumerModule } from './order-message-consumer/order-message-consumer.module';
+import * as dotenv from 'dotenv';
+const environment = process.env.NODE_ENV || 'development';
+dotenv.config({ path: `${environment}.env` });
 
 async function bootstrap() {
   const app = await NestFactory.createMicroservice(OrderMessageConsumerModule, {
     transport: Transport.RMQ,
     options: {
-      urls: ['amqp://127.0.0.1'],
+      urls: [process.env.AMQP_CONN_STRING],
       queue: QUEUES.ORDER_CREATED_QUEUE,
       queueOptions: {
         durable: true,
